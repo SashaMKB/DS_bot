c5fd9831dcf813bfbbced9e2a1f9b22536ebf03d
Alexander <73365442+SashaMKB@users.noreply.github.com>
2022-08-17 16:00:52 +0300

    add databse

--- bot/botrun.py
+++ bot/botrun.py
@@ -1,11 +1,23 @@
 import discord
 from discord.ext import commands
-import os
+import os, sqlite3
 
 file = open("TOKEN.txt", "r")
 line = file.readline()
 file.close()
 
-bot = commands.Bot(command_prefix='|')
+bot = commands.Bot(command_prefix='|', intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print("I`m on ready")
+
+    global base, cur
+    base = sqlite3.connect("Data.db")
+    cur = base.cursor()
+    if base:
+        print("DataBase connected...Ok")
+
 
 bot.run(line)
